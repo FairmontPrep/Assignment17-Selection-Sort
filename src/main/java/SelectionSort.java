a89eb42b0b2d50ace53ba6d8b479d598237684b5
Preston Fawcett <136362421+mrfawcett@users.noreply.github.com>
2024-02-06 23:09:07 -0800

    Added skeleton code

---
+++ src/main/java/SelectionSort.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class SelectionSort {
+	
+	public static void sortLowToHigh(int[] a)
+	{
+		
+	}
+	
+	public static void sortHighToLow(int[] a)
+	{
+		
+	}
+	
+	public static void sortLowToHigh(ArrayList<Comparable> a)
+	{
+		
+	}
+	
+	public static void sortHighToLow(ArrayList<Comparable> a)
+	{
+		
+	}
+
+
+}
